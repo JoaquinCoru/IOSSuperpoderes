468f1a9a1b1e819de1d818a41c0b07c4dc196c5b
Joaquín Corugedo <joaquin_coru@hotmail.com>
2022-10-17 21:44:49 +0200

    Correcciones targets de test

--- MarvelAppTests/RootViewModelTests.swift
+++ MarvelAppTests/RootViewModelTests.swift
@@ -1,13 +1,13 @@
 //
-//  MarvelAppTests.swift
+//  RootViewModelTests.swift
 //  MarvelAppTests
 //
-//  Created by Joaquín Corugedo Rodríguez on 12/10/22.
+//  Created by Joaquín Corugedo Rodríguez on 17/10/22.
 //
 
 import XCTest
 
-final class MarvelAppTests: XCTestCase {
+final class RootViewModelTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -27,7 +27,7 @@ final class MarvelAppTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
